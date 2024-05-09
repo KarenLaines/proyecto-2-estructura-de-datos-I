6a7dbc020d3934e32c6f5e6f4f3d2d6e4a9d2e4d
Jasgu097 <124702997+Jasgu097@users.noreply.github.com>
2024-05-09 00:16:13 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,72 @@
-from linked_list import LinkedList
-from circular_list import CircularList
-
-linked_list = LinkedList()
-circular_list = CircularList()
-
-linked_list.append(1)
-linked_list.append(2)
-linked_list.append(3)
-linked_list.append(4)
-linked_list.append(5)
+from PyQt6 import QtCore, QtGui, QtWidgets
 
 
-circular_list.append(1)
-circular_list.append(2)
-circular_list.append(3)
-circular_list.append(4)
-circular_list.append(5)
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(parent=self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(0, 0, 801, 561))
+        self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap("Foto1.jpeg"))
+        self.label.setScaledContents(True)
+        self.label.setObjectName("label")
+        self.pushButton = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(320, 90, 151, 31))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(100, 140, 151, 31))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(540, 140, 151, 31))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(320, 200, 151, 31))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.pushButton_5.setGeometry(QtCore.QRect(100, 350, 151, 31))
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_6 = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.pushButton_6.setGeometry(QtCore.QRect(540, 350, 211, 31))
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.pushButton_7 = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.pushButton_7.setGeometry(QtCore.QRect(260, 420, 271, 31))
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.pushButton_8 = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.pushButton_8.setGeometry(QtCore.QRect(290, 310, 201, 31))
+        self.pushButton_8.setObjectName("pushButton_8")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("Ventana Principal", "Ventana Principal"))
+        self.pushButton.setText(_translate("MainWindow", "PILA"))
+        self.pushButton_2.setText(_translate("MainWindow", "COLA"))
+        self.pushButton_3.setText(_translate("MainWindow", "LISTA ENLAZADA"))
+        self.pushButton_4.setText(_translate("MainWindow", "LISTA CIRCULAR"))
+        self.pushButton_5.setText(_translate("MainWindow", "ARBOL BINARIO"))
+        self.pushButton_6.setText(_translate("MainWindow", "ARBOL BINARIO DE BUSQUEDA"))
+        self.pushButton_7.setText(_translate("MainWindow", "LISTA CIRCULAR DOBLEMENTE ENLAZADA"))
+        self.pushButton_8.setText(_translate("MainWindow", "LISTA DOBLEMENTE ENLAZADA"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec())
